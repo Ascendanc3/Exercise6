8143366d3f7dfe056aca83f560802665a62dd844
Ananda Aditya <53120949+Ascendanc3@users.noreply.github.com>
2021-11-18 02:11:11 +0800

    Update index.js

--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -4,7 +4,7 @@ const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [users, setUsers] = useState([]);
     useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/users/")
+        fetch("https://reqres.in/api/users?page=2")
             .then(res => res.json())
             .then(
                 (data) => {
